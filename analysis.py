aa2287e29101db82cf1a33a10788bd80387a3f30
Masa <masa@datascience-lab.xyz>
2021-05-09 00:40:46 +0900

    analysis.py modified to accept start/end date of calculation

--- analysis.py
+++ analysis.py
@@ -1,9 +1,10 @@
 import datetime
+from datetime import datetime, timedelta
 import os
 from update_stock_data import stock_markets
 
 import pandas as pd
-from sqlalchemy import MetaData, Table, Column, Float, String, create_engine
+from sqlalchemy import MetaData, Table, Column, Date, Float, String, create_engine
 
 # parameters
 db_dir = os.environ['FLASK_DB_PATH']
@@ -29,7 +30,7 @@ database_file = db_dir + 'stock-indices.db'
 def init_sqlalchemy():
     metadata = MetaData()
     Nikkei225 = Table('Nikkei225', metadata,
-                      Column('date', String(10), primary_key=True),
+                      Column('date', Date, primary_key=True),
                       Column('open', Float),
                       Column('high', Float),
                       Column('low', Float),
@@ -142,40 +143,69 @@ def init_sqlalchemy():
     return engine, connection
 
 
-def setup_dataframe(engine, df):
+def create_blank_dataframe(start, end):
+    start_date = datetime.strptime(start, "%Y-%m-%d")#convert to datetime type
+    end_date = datetime.strptime(end, "%Y-%m-%d")#convert to datetime type
+
+    date = start_date
+    days_list = [start]
+
+    for i in range((end_date - start_date).days):
+        date = date + timedelta(days=1)
+        days_list.append(datetime.strftime(date, '%Y-%m-%d'))
+    df = pd.DataFrame(index=days_list, columns=[])#欠損の無い日付だけが入ったDataFrameを作成
+
+    return df
+
+
+
+def downlad_data(engine, df, start, end):
     for market in stock_markets.values():
-        globals()[market] = pd.read_sql_query('SELECT date, close FROM {}'.format(market), engine)
+        query = 'SELECT date, close FROM {} where date between "{}" and "{}"'.format(market, start, end)
+        globals()[market] = pd.read_sql_query(query, engine)
+
         close_tmp = globals()[market][['date', 'close']]
         close_tmp = close_tmp.set_index('date')
         close_tmp = close_tmp.rename(columns={'close': market})
 
-        if df.empty == True:
-            df = close_tmp
-        else:
-            df = pd.concat([df, close_tmp], axis=1)
+        df = pd.concat([df, close_tmp], axis=1)
 
-    # fill n/a data with the previous date
     df = df.sort_index(ascending=False)
-    df = df.fillna(method='bfill', limit=10)
 
     return df
 
-def analysis(df):
-    # 基準日を決める
-    start = '2020-04-01'
-    end = df.index[0]
+def data_preprocessing(df):
+    df = df.fillna(method='bfill', limit=28)#欠損値を埋める
+    missing = df.isnull().sum()
 
+    return df
+
+def analysis(df, start):
     # 基準日の値で基準化　
     for market in df.columns:
         start_value = df[market][start]
         df[market] = df[market] /start_value
 
+    result = df[:1].T
+    label_in_df = df[:1].index.values[0]
+    label_new = 'value change (%)'
+    result = result.rename(columns={label_in_df: label_new }).sort_values(label_new, ascending=False)
+
+    result_json = result.to_json()
+
+    return result_json
+
 
 def main():
+    start = "2021-04-01"
+    end = "2021-04-28"
     engine, connection = init_sqlalchemy()
-    df = pd.DataFrame()
-    df = setup_dataframe(engine, df)
-    analysis(df)
+    df = create_blank_dataframe(start, end)
+    df = downlad_data(engine, df, start, end)
+    df = data_preprocessing(df)
+    result_json = analysis(df, start)
+
+    return result_json
 
 
 if __name__ == '__main__':
